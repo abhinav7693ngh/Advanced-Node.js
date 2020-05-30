5f94f823dbb2ea99ff3a40680fb1be994fb6ed0b
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-05-30 19:11:29 +0530

    Flowing to non-flowing readStreams using pause() and read() method

--- Streams/Using_Readable_Streams.js
+++ Streams/Using_Readable_Streams.js
@@ -3,18 +3,20 @@ const fs = require('fs');
 
 const readStream = fs.createReadStream('./sample.mp4');
 
-// readStream.on('data', (chunk) => {
-//     console.log(chunk.length);
-// });
+readStream.on('data', (chunk) => {
+    console.log(chunk.length);
+});
 
-// readStream.on('end', () => {
-//     console.log('Finished');
-// });
+readStream.on('end', () => {
+    console.log('Finished');
+});
 
-// readStream.on('error', () => {
-//     console.log('Error occured');
-// });
+readStream.on('error', () => {
+    console.log('Error occured');
+});
+
+readStream.pause();
 
 process.stdin.on('data', chunk => {
-    console.log(`Echo : ${chunk}`);
+    readStream.read();
 });
